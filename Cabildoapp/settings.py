94bd0f33acc890b6882ea9c03fd5201d4a975d5a
JorgeJativa97 <georgoandres.jm@gmail.com>
2025-12-30 10:49:55 -0500

    agregando configuracion de cors

--- Cabildoapp/settings.py
+++ Cabildoapp/settings.py
@@ -200,6 +200,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5174",
     "http://localhost:5174",
     "http://localhost:3000",
+    "http://192.168.50.90:3000",
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
